2b9591eed03c90c7edb92bccc7dc4962dc16c885
nngouyenngoc913 <nngouyenngoc@gmail.com>
2026-05-23 12:41:02 +0700

    Update data.js

--- data.js
+++ data.js
@@ -63,14 +63,7 @@ const siteData = {
                 description: "Tìm hiểu cách sử dụng mô hình cây quyết định và AI để tự động hóa định khoản, loại bỏ lối mòn tư duy.", 
                 image: "https://images.unsplash.com/photo-1551288049-bebda4e38f71?w=800", 
                 tag: "AI & Data",
-                file: "posts/post-1.md"
-            },
-            { 
-                title: "Quản trị Kinh doanh: Từ Bảng cân đối đến Vận hành", 
-                description: "Đọc vị sức khỏe tài chính và chiến lược phân bổ nguồn lực của doanh nghiệp thông qua Balance Sheet.", 
-                image: "https://images.unsplash.com/photo-1460925895917-afdab827c52f?w=800", 
-                tag: "Management",
-                file: "posts/post-2.md"
+                file: "posts/md1.md"
             }
         ]
     }
